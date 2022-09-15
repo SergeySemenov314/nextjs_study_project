8b14ffa3cc8c7b9355605c81ac5798a6bc81a4d7
Sergey Semenov <s3menov.semenow@yandex.ru>
2022-09-15 11:05:20 +0300

    added 404 page and finished

---
+++ pages/404.js
@@ -0,0 +1,13 @@
+import MainContainer from "../components/MainContainer";
+
+const Error = () => {
+    return (
+        <MainContainer>
+            <div>
+                <h1>Страница не найдена</h1>
+            </div>
+        </MainContainer>
+    );
+};
+
+export default Error;
